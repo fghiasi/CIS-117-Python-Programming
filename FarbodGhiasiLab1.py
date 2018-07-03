0ed9b08478bfd17487666020dbd3eea6a5aa8175
Farbod Ghiasi <farbodghiasi@my.smccd.edu>
2018-07-03 13:34:22 -0700

    Adding Lab number one and two

---
+++ FarbodGhiasiLab1.py
@@ -0,0 +1,65 @@
+###########################################################
+#
+# Name: Farbod Ghiasi
+# Course: CIS 117 Python Programming Lab #1
+# Term: Summer
+# Lab Solution file: FarbodGhiasiLab1.py
+# Development Env: Ubuntu 17.10
+# Version: Python 3.6.3
+# Date: Jun/19/2018
+# Description: This Program will print
+#              My last name.
+#              And my CSM ID # (G number).
+#              And any two details from CIS 117 course
+#              Syllabus and three Details from the resource
+#              module Lab Coding Standard. At the
+#              end a copy of the output is presented.
+#              Indentation is space.
+#
+###########################################################
+
+# Print Hello World!
+
+print("Hello World!\n")
+
+# Print My Last name and G number.
+
+print("My Last Name is Ghiasi.\n")
+print("My G number is G01074997.\n")
+
+# Print two important things from syllabus.
+
+print("Syllabus detail #1: Please use public discussion to ask a question or "
+      "discuss a problem. That helps other because they might have same "
+      "question. Also, for grading question please use private messaging.\n")
+print("Syllabus detail #2: If you turn in you Lab late, you will loose "
+      "2 points per day and if the assignment is late more than 2 days. It "
+      "will not be accepted. There won't be a makeup test or exam "
+      "without prior permission.\n")
+
+# Print three important things from Lab coding Style.
+
+print("Specific lab detail #1: In the source code we are supposed to "
+      "write a descriptive comment explaining a block of code that does "
+      "major data manipulation or error processing.\n")
+print("Specific lab detail #2: use CamelCase style for naming all "
+      "classes for example: class EmployeeIncome\n")
+print("Specific lab detail #3: We should try to use meaningful "
+      "identifiers that are taken from the problem domain for all names."
+      "A name will be used for one purpose you can't use it for more than "
+      "one purpose.\n")
+
+# A commented out copy of the output.
+
+'''
+Syllabus detail #1: Please use public discussion to ask a question or discuss a problem. That helps other because they might have same question. Also, for grading question please use private messaging.
+
+Syllabus detail #2: If you turn in you Lab late, you will loose 2 points per day and if the assignment is late more than 2 days. It will not be accepted. There won't be a makeup test or exam without prior permission.
+
+Specific lab detail #1: In the source code we are supposed to write a descriptive comment explaining a block of code that does major data manipulation or error processing.
+
+Specific lab detail #2: use CamelCase style for naming all classes for example: class EmployeeIncome
+
+Specific lab detail #3: We should try to use meaningful identifiers that are taken from the problem domain for all names.A name will be used for one purpose you can't use it for more than one purpose.
+
+'''
---
+++ FarbodGhiasiLab2.py
@@ -0,0 +1,161 @@
+###########################################################
+#
+# Name: Farbod Ghiasi
+# Course: CIS 117 Python Programming Lab #2
+# Lab 2: Loops and Functions
+# Term: Summer
+# Lab Solution file: FarbodGhiasiLab2.py
+# Development Env: Ubuntu 18.04
+# Version: Python 3.6.5
+# Date: Jun/24/2018
+# Description: This program using functions, and loops
+#              is trying to ask a user to enter a password
+#               then validate it using these conditions:
+#               1- is it 8 char long. 2- has at least one uppercase
+#               and lowercase letter 3- has at least one digit. 4-
+#               does the 2 entered pass match. If not as user to try
+#               again.
+#
+###########################################################
+
+
+# Return true if pass satisfies min char length requirement
+def has_min_length(password):
+
+    min_length = 8
+
+    if len(password) < min_length :
+
+        display_error("=> The password must be at least 8 characters long.")
+
+    return len(password) >= min_length
+
+
+# Return True if pass has at least one upper case
+def has_upper_case(password):
+
+    for letter in password:
+
+        if letter.isupper():
+
+            return True
+
+    display_error("=> The password must have at least one uppercase.")
+
+    return False
+
+
+# Return True if pass has at least one lower case
+def has_lower_case(password):
+
+    for letter in password:
+
+        if letter.islower():
+
+            return True
+
+    display_error("=> The password must have at least one lowercase.")
+    return False
+
+
+# Return True if pass has at least one digit
+def has_digit(password):
+
+    for letter in password:
+
+        if letter.isdigit():
+
+            return True
+
+    display_error("=> The password must have at least one digit.")
+
+    return False
+
+
+# ask user to re-enter pass and return True if it matched
+def is_match(password):
+
+    re_enter_password = get_password("\n=> Please re-enter the password to "
+                                     "match.")
+
+    if password != re_enter_password:
+
+        display_error("The two password didn't match.")
+
+    return password == re_enter_password
+
+
+# Return True if pass meet all requirements
+def has_requirements(password):
+
+    return has_lower_case(password) and \
+           has_upper_case(password) and \
+           has_digit(password) and \
+           has_min_length(password) and \
+           is_match(password)
+
+
+# Get pass. show specific message.
+def get_password(display_message):
+    return input(display_message + "\n")
+
+
+def display_error(error_text):
+
+    print("\n" + error_text + "\n")
+
+
+# Print requirements.
+print("=> The password must be at least 8 characters long.\n"
+      "=> The password must have at least one uppercase and one "
+      "lowercase letter.\n=> The password must have at least one digit.\n")
+
+
+# get pass for the first time.
+password = get_password("Please Enter a password considering above "
+                        "conditions:")
+
+# ask user to correct the pass to meet requirement
+while not has_requirements(password):
+    password = get_password("Try again: Please Enter a password.")
+
+# Print the pass will work.
+print("That pair of passwords will work.")
+
+
+'''
+=> The password must be at least 8 characters long.
+=> The password must have at least one uppercase and one lowercase letter.
+=> The password must have at least one digit.
+
+Please Enter a password considering above conditions:
+farbod
+
+=> The password must have at least one uppercase.
+
+Try again: Please Enter a password.
+Farbod
+
+=> The password must have at least one digit.
+
+Try again: Please Enter a password.
+Farbod1
+
+=> The password must be at least 8 characters long.
+
+Try again: Please Enter a password.
+Farbod12
+
+=> Please re-enter the password to match.
+Farbod21
+
+The two password didn't match.
+
+Try again: Please Enter a password.
+Farbod12
+
+=> Please re-enter the password to match.
+Farbod12
+That pair of passwords will work.
+
+'''
